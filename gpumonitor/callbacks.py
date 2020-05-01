3609bcef7ac06482db0032e76925cab39e72afd0
Raphael Meudec <raphaelm@sicara.com>
2020-05-01 11:37:42 +0200

    Add PyTorch Lightning callback

--- gpumonitor/callbacks.py
+++ gpumonitor/callbacks.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import pytorch_lightning as pl
 
 from gpumonitor.monitor import GPUStatMonitor
 
@@ -12,6 +13,20 @@ class TFGpuMonitorCallback(tf.keras.callbacks.Callback):
         self.monitor = GPUStatMonitor(self.delay)
 
     def on_epoch_end(self, epoch, logs=None):
+        self.monitor.stop()
+        print("")
+        self.monitor.display_average_stats_per_gpu()
+
+
+class PyTorchGpuMonitorCallback(pl.callbacks.base.Callback):
+    def __init__(self, delay):
+        super(PyTorchGpuMonitorCallback, self).__init__()
+        self.delay = delay
+
+    def on_epoch_start(self, trainer, pl_module):
+        self.monitor = GPUStatMonitor(self.delay)
+
+    def on_epoch_end(self, trainer, pl_module):
         self.monitor.stop()
         print("")
         self.monitor.display_average_stats_per_gpu()
