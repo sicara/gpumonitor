94aa7f46f3b553899f4b2ed5818c4c093cc8d543
Raphael Meudec <raphaelm@sicara.com>
2020-05-01 17:11:58 +0200

    Fix non ASCII character to build library

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="GPU Monitoring Callbacks for TensorFlow and PyTorch Lightning",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="Raphaël Meudec",
+    author="Raphael Meudec",
     author_email="raphael.meudec@gmail.com",
     url="https://github.com/sicara/gpumonitor",
     license="MIT",
